986a0143cd22a934713bed6e3157e17d116ff69d
ukieTux <ukie.tux@gmail.com>
2021-01-11 11:25:15 +0800

    Fix issue in file picker in release mode

--- android/app/src/main/kotlin/com/codexlogs/zeinholistic/MainActivity.kt
+++ android/app/src/main/kotlin/com/codexlogs/zeinholistic/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.codexlogs.zeinholistic
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
+
+
