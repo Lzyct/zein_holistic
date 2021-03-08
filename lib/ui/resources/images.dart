cb9adb5cbabefa8d8d23471d66fee9c65fb544cc
ukieTux <hey.mudassir@gmail.com>
2021-03-08 17:54:30 +0800

    Update assets structure

--- lib/ui/resources/images.dart
+++ lib/ui/resources/images.dart
@@ -1,8 +1,8 @@
 class Images {
-  static String icLogo = "images/ic_logo.svg";
-  static String icLogoText = "images/ic_logo_text.svg";
-  static String icLogoTextAlt = "images/ic_logo_text_alt.svg";
-  static String imgNoData = "images/img_no_data.svg";
-  static String icMan = "images/ic_man.svg";
-  static String icWoman = "images/ic_woman.svg";
+  static String icLogo = "assets/images/ic_logo.svg";
+  static String icLogoText = "assets/images/ic_logo_text.svg";
+  static String icLogoTextAlt = "assets/images/ic_logo_text_alt.svg";
+  static String imgNoData = "assets/images/img_no_data.svg";
+  static String icMan = "assets/images/ic_man.svg";
+  static String icWoman = "assets/images/ic_woman.svg";
 }
