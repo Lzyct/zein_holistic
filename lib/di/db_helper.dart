1cd7adb691153211d1345291db767b9c2d6f3650
ukieTux <hey.mudassir@gmail.com>
2021-03-16 11:15:37 +0800

    replace sqflite to sqflite_ffi

--- lib/di/db_helper.dart
+++ lib/di/db_helper.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:zein_holistic/di/di.dart';
 import 'package:zein_holistic/ui/resources/resources.dart';
 import 'package:zein_holistic/utils/utils.dart';
@@ -27,7 +28,8 @@ class DbHelper {
 
     logs("Path DB -> $path");
     sl<PrefManager>().setDbPath(path);
-    var theDb = await openDatabase(path, version: 1, onCreate: onCreate);
+    var theDb = await databaseFactoryFfi.openDatabase(path);
+    onCreate(theDb, 1);
     return theDb;
   }
 
