b2543a33e716c710b93dd15f48b6752cbe0ad6b0
ukieTux <hey.mudassir@gmail.com>
2021-05-05 11:49:52 +0800

    Update add patient page

--- lib/ui/pages/patient/add/add_patient_page.dart
+++ lib/ui/pages/patient/add/add_patient_page.dart
@@ -57,145 +57,168 @@ class _AddPatientPageState extends State<AddPatientPage> {
   @override
   Widget build(BuildContext context) {
     return Parent(
-      appBar: context.appBar(title: Strings.addPatient),
-      child: Scrollbar(
-        child: SingleChildScrollView(
-          child: BlocListener(
-            bloc: _addPatientBloc,
-            listener: (_, dynamic state) {
-              switch (state.status) {
-                case Status.LOADING:
-                  {
-                    Strings.pleaseWait.toToastLoading();
-                  }
-                  break;
-                case Status.ERROR:
-                  {
-                    state.message.toString().toToastError();
-                  }
-                  break;
-                case Status.SUCCESS:
-                  {
-                    Strings.successSaveData.toToastSuccess();
-                    Navigator.pop(context);
-                  }
-                  break;
-              }
-            },
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextF(
-                    hint: Strings.name,
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.text,
-                    controller: _conName,
-                    curFocusNode: _fnName,
-                    nextFocusNode: _fnDateBirth,
-                    validator: (String? value) =>
-                        value!.isEmpty ? Strings.errorEmpty : null,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        Strings.sex,
-                        style: TextStyles.textHint,
+      appBar: context.appBar(),
+      isScroll: false,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: Dimens.maxWidth),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            CustomToolbar(title: Strings.addPatient),
+            Scrollbar(
+              child: SingleChildScrollView(
+                child: BlocListener(
+                  bloc: _addPatientBloc,
+                  listener: (_, dynamic state) {
+                    switch (state.status) {
+                      case Status.LOADING:
+                        {
+                          Strings.pleaseWait.toToastLoading();
+                        }
+                        break;
+                      case Status.ERROR:
+                        {
+                          state.message.toString().toToastError();
+                        }
+                        break;
+                      case Status.SUCCESS:
+                        {
+                          Strings.successSaveData.toToastSuccess();
+                          Navigator.pop(context);
+                        }
+                        break;
+                    }
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: Responsive.isDesktop(context)
+                            ? context.dp36()
+                            : context.dp4()),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextF(
+                            hint: Strings.name,
+                            textInputAction: TextInputAction.next,
+                            keyboardType: TextInputType.text,
+                            controller: _conName,
+                            curFocusNode: _fnName,
+                            nextFocusNode: _fnDateBirth,
+                            validator: (String? value) =>
+                                value!.isEmpty ? Strings.errorEmpty : null,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                Strings.sex,
+                                style: TextStyles.textHint,
+                              ),
+                              SizedBox(width: Dimens.space16),
+                              FlutterToggleTab(
+                                  labels: _sex,
+                                  width: Dimens.space16,
+                                  height: Dimens.height35,
+                                  initialIndex: 0,
+                                  selectedLabelIndex: (index) {
+                                    _selectedSex = _sex[index];
+                                  },
+                                  selectedTextStyle: TextStyles.white,
+                                  borderRadius: Dimens.space16,
+                                  unSelectedTextStyle: TextStyles.primary),
+                            ],
+                          ).margin(
+                              edgeInsets: EdgeInsets.symmetric(
+                                  vertical: Dimens.space8)),
+                          Row(
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: TextF(
+                                  hint: Strings.dateBirth,
+                                  textInputAction: TextInputAction.next,
+                                  controller: _conDateBirth,
+                                  curFocusNode: DisableFocusNode(),
+                                  nextFocusNode: _fnAddress,
+                                  validator: (String? value) => value!.isEmpty
+                                      ? Strings.errorEmpty
+                                      : null,
+                                  onTap: () async {
+                                    var _result = await context.datePicker(
+                                        currentDate:
+                                            _conDateBirth.text.isNotEmpty
+                                                ? _conDateBirth.text.toDate()
+                                                : null);
+                                    if (_result != null) {
+                                      _conDateBirth.text =
+                                          _result.toString().toStringDate();
+                                      _conAge.text = calculateAge(_result);
+                                    }
+                                  },
+                                ),
+                              ),
+                              SizedBox(width: Dimens.space16),
+                              Expanded(
+                                flex: 2,
+                                child: TextF(
+                                  textInputAction: TextInputAction.next,
+                                  curFocusNode: DisableFocusNode(),
+                                  controller: _conAge,
+                                  hint: Strings.age,
+                                  validator: (String? value) => value!.isEmpty
+                                      ? Strings.errorEmpty
+                                      : null,
+                                ),
+                              ),
+                            ],
+                          ),
+                          TextF(
+                            hint: Strings.address,
+                            textInputAction: TextInputAction.next,
+                            controller: _conAddress,
+                            curFocusNode: _fnAddress,
+                            nextFocusNode: _fnPhoneNumber,
+                            validator: (String? value) =>
+                                value!.isEmpty ? Strings.errorEmpty : null,
+                          ),
+                          TextF(
+                            hint: Strings.phoneNumber,
+                            textInputAction: TextInputAction.done,
+                            keyboardType: TextInputType.phone,
+                            controller: _conPhoneNumber,
+                            inputFormatter: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            curFocusNode: _fnPhoneNumber,
+                            validator: (String? value) =>
+                                value!.isEmpty ? Strings.errorEmpty : null,
+                          ),
+                          SizedBox(height: Dimens.space16),
+                          Button(
+                            width: double.infinity,
+                            title: Strings.save,
+                            color: Palette.colorPrimary,
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                var _params = {
+                                  'name': _conName.text.toString(),
+                                  'sex': _selectedSex,
+                                  'dateBirth': _conDateBirth.text.toString(),
+                                  'address': _conAddress.text.toString(),
+                                  'phoneNumber': _conPhoneNumber.text.toString()
+                                };
+                                _addPatientBloc.addPatient(_params);
+                              }
+                            },
+                          )
+                        ],
                       ),
-                      SizedBox(width: Dimens.space16),
-                      FlutterToggleTab(
-                          labels: _sex,
-                          width: Dimens.toggle,
-                          height: Dimens.toggle,
-                          initialIndex: 0,
-                          selectedLabelIndex: (index) {
-                            _selectedSex = _sex[index];
-                          },
-                          selectedTextStyle: TextStyles.white,
-                          borderRadius: Dimens.space16,
-                          unSelectedTextStyle: TextStyles.primary),
-                    ],
-                  ).margin(
-                      edgeInsets: EdgeInsets.symmetric(vertical: Dimens.space8)),
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: TextF(
-                          hint: Strings.dateBirth,
-                          textInputAction: TextInputAction.next,
-                          controller: _conDateBirth,
-                          curFocusNode: DisableFocusNode(),
-                          nextFocusNode: _fnAddress,
-                          validator: (String? value) =>
-                              value!.isEmpty ? Strings.errorEmpty : null,
-                          onTap: () async {
-                            var _result = await context.datePicker(
-                                currentDate: _conDateBirth.text.isNotEmpty
-                                    ? _conDateBirth.text.toDate()
-                                    : null);
-                            if (_result != null) {
-                              _conDateBirth.text =
-                                  _result.toString().toStringDate();
-                              _conAge.text = calculateAge(_result);
-                            }
-                          },
-                        ),
-                      ),
-                      SizedBox(width: Dimens.space16),
-                      Expanded(
-                        flex: 2,
-                        child: TextF(
-                          textInputAction: TextInputAction.next,
-                          curFocusNode: DisableFocusNode(),
-                          controller: _conAge,
-                          hint: Strings.age,
-                          validator: (String? value) =>
-                              value!.isEmpty ? Strings.errorEmpty : null,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  TextF(
-                    hint: Strings.address,
-                    textInputAction: TextInputAction.next,
-                    controller: _conAddress,
-                    curFocusNode: _fnAddress,
-                    nextFocusNode: _fnPhoneNumber,
-                    validator: (String? value) =>
-                        value!.isEmpty ? Strings.errorEmpty : null,
-                  ),
-                  TextF(
-                    hint: Strings.phoneNumber,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.phone,
-                    controller: _conPhoneNumber,
-                    inputFormatter: [FilteringTextInputFormatter.digitsOnly],
-                    curFocusNode: _fnPhoneNumber,
-                    validator: (String? value) =>
-                        value!.isEmpty ? Strings.errorEmpty : null,
-                  ),
-                  SizedBox(height: Dimens.space16),
-                  Button(
-                    title: Strings.save,
-                    color: Palette.colorPrimary,
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        var _params = {
-                          'name': _conName.text.toString(),
-                          'sex': _selectedSex,
-                          'dateBirth': _conDateBirth.text.toString(),
-                          'address': _conAddress.text.toString(),
-                          'phoneNumber': _conPhoneNumber.text.toString()
-                        };
-                        _addPatientBloc.addPatient(_params);
-                      }
-                    },
-                  )
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
